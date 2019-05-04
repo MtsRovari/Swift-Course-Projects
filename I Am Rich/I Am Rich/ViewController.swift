a64e676adf7d9dfb71d0b1ba4a1df5baaac57a54
Mateus Rovari <mateusrovari@Mateuss-MacBook-Pro.local>
2019-05-03 23:23:55 -0300

    i am rich app

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Mateus Rovari on 02/05/19.
+//  Copyright © 2019 Mateus Rovari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
